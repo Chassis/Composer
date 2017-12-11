9ef5cc7da46a80a41eb6fd2cf78fc34f531776ac
Bronson Quick <bronson@bronsonquick.com.au>
2017-12-11 17:19:01 +1000

    Remove composer

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -9,26 +9,45 @@ class composer (
 		$php_package = "php${composer_config[php]}"
 	}
 
+	if ( ! empty( $composer_config[disabled_extensions] ) and 'composer' in $composer_config[disabled_extensions] ) {
+		$package = absent
+	} else {
+		$package = latest
+	}
+
 	if ! defined( Package["${php_package}-dev"] ) {
 		package { "${php_package}-dev":
-			ensure  => latest,
+			ensure  => $package,
 			require => Package["${php_package}-fpm"]
 		}
 	}
 
 	if ! defined( Package['php-pear'] ) {
 		package { 'php-pear':
-			ensure  => latest,
+			ensure  => $package,
 			require => Package["${php_package}-dev"]
 		}
 	}
 
-	exec { 'install composer':
-		path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-		environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
-		command     => 'curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer',
-		require     => [ Package['curl'], Package['php-pear'] ],
-		unless      => 'test -f /usr/bin/composer',
+	if ( $package == 'latest' ) {
+		exec { 'install composer':
+			path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
+			command     =>
+				'curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer',
+			require     => [ Package['curl'], Package['php-pear'] ],
+			unless      => 'test -f /usr/bin/composer',
+		}
+	} else {
+		file { 'remove composer cache':
+			ensure => absent,
+			path   => '/home/vagrant/.cache/composer',
+			force  => true
+		}
+		file { 'remove composer':
+			ensure => absent,
+			path   => '/usr/bin/composer',
+		}
 	}
 }
 
