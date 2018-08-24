5bd15b21f4c691d52b41b3b0e1e9ba8f2525734e
Bronson Quick <bronson@bronsonquick.com.au>
2018-08-24 16:51:10 +1000

    Make sure composer is installed before WordPress is installed with WP-CLI

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -22,23 +22,17 @@ class composer (
 		$php_dir = "php/${short_ver}"
 	}
 
+	$php_cli = "php${short_ver}-cli"
+
 	if ( ! empty( $config[disabled_extensions] ) and 'composer' in $config[disabled_extensions] ) {
 		$package = absent
 	} else {
 		$package = latest
 	}
 
-	if ! defined( Package["${php_package}-dev"] ) {
-		package { "${php_package}-dev":
+	if ! defined( Package["${php_cli}"] ) {
+		package { "${php_cli}":
 			ensure  => $package,
-			require => Package["${php_package}-fpm"]
-		}
-	}
-
-	if ! defined( Package['php-pear'] ) {
-		package { 'php-pear':
-			ensure  => $package,
-			require => Package["${php_package}-dev"]
 		}
 	}
 
@@ -48,7 +42,7 @@ class composer (
 			environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
 			command     =>
 				'curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer',
-			require     => [ Package['curl'], Package['php-pear'] ],
+			require     => [ Package['curl'], Package[$php_cli] ],
 			unless      => 'test -f /usr/bin/composer',
 		}
 	} else {
