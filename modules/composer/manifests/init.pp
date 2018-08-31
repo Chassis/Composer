b59621f91fb76454cb998eb2a26b0ff1889ce400
Bronson Quick <bronson@bronsonquick.com.au>
2018-08-31 18:07:33 +1000

    Refactor the dependencies

--- modules/composer/manifests/init.pp
+++ modules/composer/manifests/init.pp
@@ -2,15 +2,6 @@
 class composer (
 	$config,
 ) {
-	# Puppet 3.8 doesn't have the .each function and we need an alternative.
-	define install {
-		exec { "Installing Composer ${name}":
-			environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
-			path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-			cwd         => "/vagrant/${name}",
-			command     => 'composer install',
-		}
-	}
 
 	$version = $config[php]
 
@@ -33,6 +24,17 @@ class composer (
 	}
 
 	$php_cli = "php${short_ver}-cli"
+
+	# Puppet 3.8 doesn't have the .each function and we need an alternative.
+	define install {
+		exec { "Installing Composer ${name}":
+		  environment => [ 'COMPOSER_HOME=/usr/bin/composer' ],
+		  path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+		  cwd         => "/vagrant/${name}",
+		  command     => 'composer install',
+		  require     => [ Exec['install composer'] ],
+		}
+	}
 
 	if ( ! empty( $config[disabled_extensions] ) and 'composer' in $config[disabled_extensions] ) {
 		$package = absent
